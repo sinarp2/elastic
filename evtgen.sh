a82c493cd45896394c2d57713bf107930dceb836
EricNoh <dosungroh@inslab.co.kr>
2018-02-12 14:07:56 +0900

    es_tools init

--- evtgen.sh
+++ evtgen.sh
@@ -43,6 +43,7 @@ echo "cat $1 | nc $UDP $HOST $PORT"
 while true; do
     while IFS='' read -r line || [[ -n "$line" ]]; do
         now=$(date +"%d/%b/%Y:%H:%M:%S +0000")
+        # 첫 번 째 컬럼의 날짜시간을 실 시간 날짜시간으로 변경한다. - 첫 번 째 컬럼이 오직 날짜시간인 경우에만 해당
         echo "$line" | sed -e "s,\([0-9]\)\{2\}\/\([a-zA-Z]\)\{3\}\/\([0-9]\)\{4\}\:\([0-9]\)\{2\}\:\([0-9]\)\{2\}\:\([0-9]\)\{2\} +0000,${now}," | nc $UDP $HOST $PORT
         sleep $INTERVAL
     done < "$1"
