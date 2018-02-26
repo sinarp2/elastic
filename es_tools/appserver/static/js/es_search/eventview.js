34de2a622b001282b8e085c50d7985dc0df2a965
EricNoh <dosungroh@inslab.co.kr>
2018-02-26 09:36:36 +0900

    0226 월요일 09시

--- es_tools/appserver/static/js/es_search/eventview.js
+++ es_tools/appserver/static/js/es_search/eventview.js
@@ -9,6 +9,8 @@ define([
     "text!../app/Clay/html/es_eventview.html"
 ], function ($, template) {
 
+    // table-row-expanding 테이블 drilldown 시 추가 해야할 css 클래스
+
     function EventView(parent) {
         let _el = template
         let _parent = parent
@@ -18,24 +20,30 @@ define([
             $(_parent).append(_el)
             var $tr = $(_parent).find('[es-repeat]')
             var trHtml = $tr.html().slice(0)
-            var m = trHtml.match('/(\{\{\S+\}\})/g')
-            console.log(m)
+            //var m = trHtml.match('/(\{\{\S+\}\})/g')
+            //console.log(m)
             var tb = $tr.parent()
             $tr.html('')
             for (var i = 0; i < hits.length; i++) {
-                var source = hits[i]['_source']
                 var $row = $tr.clone()
                 var html = trHtml.slice(0)
-                html = html.replace('{{line-num}}', '<!--{{line-num}}-->' + (i + 1))
-                html = html.replace('{{date}}', '<!--{{date}}-->' + source['@timestamp'])
-                html = html.replace('{{event_data}}', '<!--{{event_data}}-->' + JSON.stringify(source))
+                $.each(hits[i], function(key, value) {
+                    html = bind(key, value, html)
+                })
+                // html = html.replace('{{line-num}}', '<!--{{line-num}}-->' + (i + 1))
+                // html = html.replace('{{date}}', '<!--{{date}}-->' + source['@timestamp'])
+                // html = html.replace('{{event_data}}', '<!--{{event_data}}-->' + JSON.stringify(source))
                 $(tb).append($row.html(html))
             }
             $tr.hide()
             return this
         }
-
     }
 
     return EventView
+
+    function bind(key, value, html) {
+        console.log('key', key, value)
+        return html
+    }
 })
